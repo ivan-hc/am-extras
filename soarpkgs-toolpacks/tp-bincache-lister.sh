1a5e650ebe8b706cd750e22874c113f5a410c85a
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-02-18 10:35:18 +0100

    Update tp-bincache-lister.sh

--- soarpkgs-toolpacks/tp-bincache-lister.sh
+++ soarpkgs-toolpacks/tp-bincache-lister.sh
@@ -2,10 +2,12 @@
 
 architectures="x86_64 aarch64"
 for arch in $architectures; do
+	echo "| appname | description | site | download | version |" > "$arch".md
+	echo "| ------- | ----------- | ---- | -------- | ------- |" >> "$arch".md
 	curl -Ls "https://raw.githubusercontent.com/pkgforge/metadata/main/bincache/data/$arch-Linux.json" \
 		| grep "{\|\"pkg_name\"\|\"description\"\|\"pkg_webpage\"\|\"download_url\"\|\"version\"" \
 		| sed 's/,$/ | /g' | xargs | tr '{}' '\n' | sed 's/ pkg_name:/|/g' | sort -u \
 		| sed 's/description:/ | /g; s/pkg_webpage:/ | /g; s/download_url:/ | /g; s/version:/ | /g; s/  / /g; s/  / /g' \
 		| sed 's/| |/|/g' | awk -F'|' '{print $1 $2 $4 $3 $6 $5}' | sed 's/  / | /g; s/^/|/g; s/$/|/g' \
-		| grep -v "| |\|||\||[a-zA-Z0-9]"> "$arch".md
+		| grep -v "| |\|||\||[a-zA-Z0-9]" >> "$arch".md
 done
