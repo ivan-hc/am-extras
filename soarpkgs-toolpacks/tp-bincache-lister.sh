5a8591416f9f2be4fa9461eea1c9d625e9b8234a
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-02-20 00:10:46 +0100

    Update tp-bincache-lister.sh

--- soarpkgs-toolpacks/tp-bincache-lister.sh
+++ soarpkgs-toolpacks/tp-bincache-lister.sh
@@ -5,9 +5,9 @@ for arch in $architectures; do
 	echo "| appname | description | site | download | version |" > "$arch".md
 	echo "| ------- | ----------- | ---- | -------- | ------- |" >> "$arch".md
 	curl -Ls "https://github.com/pkgforge/bincache/releases/download/metadata/$arch-Linux.json" \
-		| grep "{\|\"pkg_name\"\|\"description\"\|\"pkg_webpage\"\|\"ghcr_blob\"\|\"version\"" \
+		| grep "{\|\"pkg_name\"\|\"description\"\|\"pkg_webpage\"\|\"download_url\"\|\"version\"" \
 		| sed 's/,$/ | /g' | xargs | tr '{}' '\n' | sed 's/ pkg_name:/|/g' | sort -u \
-		| sed 's/description:/ | /g; s/pkg_webpage:/ | /g; s/ghcr_blob:/ | /g; s/version:/ | /g; s/  / /g; s/  / /g' \
+		| sed 's/description:/ | /g; s/pkg_webpage:/ | /g; s/download_url:/ | /g; s/version:/ | /g; s/  / /g; s/  / /g' \
 		| sed 's/| |/|/g' | awk -F'|' '{print $1 $2 $4 $3 $6 $5}' | sed 's/  / | /g; s/^/|/g; s/$/|/g' \
 		| grep -v "| |\|||\||[a-zA-Z0-9]" >> "$arch".md
 done
