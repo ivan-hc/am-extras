549cd3e0f08c541fb20b0ee3f2e277cec20afa32
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-02-22 06:31:30 +0100

    Update lister.sh

--- appbundlehub/lister.sh
+++ appbundlehub/lister.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-architectures="x86_64 aarch64"
+architectures="x86_64"
 for arch in $architectures; do
 	echo "| appname | description | site | download | version |" > "$arch".md
 	echo "| ------- | ----------- | ---- | -------- | ------- |" >> "$arch".md
-	curl -Ls "https://github.com/pkgforge/bincache/releases/download/metadata/$arch-Linux.json" | jq '.' \
-		| grep "{\|\"pkg_name\"\|\"description\"\|\"pkg_webpage\"\|\"download_url\"\|\"version\"" \
-		| sed 's/,$/ | /g' | xargs | tr '{}' '\n' | sed 's/ pkg_name:/|/g' | sort -u \
-		| sed 's/description:/ | /g; s/pkg_webpage:/ | /g; s/download_url:/ | /g; s/version:/ | /g; s/  / /g; s/  / /g' \
-		| sed 's/| |/|/g' | awk -F'|' '{print $1 $2 $4 $3 $6 $5}' | sed 's/  / | /g; s/^/|/g; s/$/|/g' \
-		| grep -v "| |\|||\||[a-zA-Z0-9]" >> "$arch".md
+	curl -Ls "$(curl -Ls https://api.github.com/repos/xplshn/AppBundleHUB/releases/latest | sed 's/[()",{} ]/\n/g' | grep -oi "http.*download.*metadata_$arch-Linux.json$" | head -1)" \
+	| jq -r '
+	.appbundlehub[] | 
+	.version |= (if type == "string" then sub("\\(may be inaccurate\\)"; "") else . end) |
+	.pkg_name |= ascii_downcase |
+	"| \(.pkg_name) | \(.description) | \(.homepage) | \(.download_url) | \(.version)|"' >> "$arch".md
 done
