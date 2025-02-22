62af0b98d05ed4eda5fc35afed3106e01a316e92
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-02-22 19:48:23 +0100

    Update lister.sh

--- testing/lister.sh
+++ testing/lister.sh
@@ -4,7 +4,7 @@ architectures="x86_64 aarch64"
 for arch in $architectures; do
 	echo "| appname | description | site | download | version |" > "$arch".md
 	echo "| ------- | ----------- | ---- | -------- | ------- |" >> "$arch".md
-	curl -Ls "https://github.com/pkgforge/bincache/releases/download/metadata/$arch-Linux.json" | jq '.' \
+	curl -Ls "https://github.com/pkgforge/pkgcache/releases/download/metadata/$arch-Linux.json" | jq '.' \
 		| grep "{\|\"pkg_name\"\|\"description\"\|\"pkg_webpage\"\|\"download_url\"\|\"version\"" \
 		| sed 's/,$/ | /g' | xargs | tr '{}' '\n' | sed 's/ pkg_name:/|/g' | sort -u \
 		| sed 's/description:/ | /g; s/pkg_webpage:/ | /g; s/download_url:/ | /g; s/version:/ | /g; s/  / /g; s/  / /g' \
