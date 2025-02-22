3c64e6244ba6add8e5a613ff4b9acbe95b08276a
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-02-22 14:52:19 +0100

    Update lister.sh

--- testing/lister.sh
+++ testing/lister.sh
@@ -10,4 +10,10 @@ for arch in $architectures; do
 		| sed 's/description:/ | /g; s/pkg_webpage:/ | /g; s/download_url:/ | /g; s/version:/ | /g; s/  / /g; s/  / /g' \
 		| sed 's/| |/|/g' | awk -F'|' '{print $1 $2 $4 $3 $6 $5}' | sed 's/  / | /g; s/^/|/g; s/$/|/g' \
 		| grep -v "| |\|||\||[a-zA-Z0-9]" >> "$arch".md
+	appnames=$(awk -F'|' '{print $2}' "$arch".md | grep -v -- "appname\|---" | tr ' ' '-' | sed -- 's/^-//g; s/-$//g')
+	for app in $appnames; do
+		app_lower=$(echo "$app" | tr '[:upper:]' '[:lower:]')
+		pure_app=$(echo "$app" | tr '-' ' ')
+		sed -i "s/^| $pure_app |/| $app_lower |/g" "$arch".md
+	done
 done
