0b3734b91e0d433b5529f3d05d28f68e3646d760
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-02-24 12:42:20 +0100

    Update lister.sh

--- appbundlehub/lister.sh
+++ appbundlehub/lister.sh
@@ -9,7 +9,8 @@ for arch in $architectures; do
 		| sed 's/,$/ | /g' | xargs | tr '{}' '\n' | sed 's/ pkg_name:/|/g' | sort -u \
 		| sed 's/description:/ | /g; s/homepage:/ | /g; s/download_url:/ | /g; s/version:/ | /g; s/  / /g; s/  / /g; s/ (may be inaccurate)//g' \
 		| sed 's/| |/|/g' | awk -F'|' '{print $1 $2 $3 $6 $5 $4}' | sed 's/  / | /g; s/^/|/g; s/$/|/g' \
-		| grep -v "| |\|||\||[a-zA-Z0-9]" >> "$arch".md
+		| grep -v "| |\|||\||[a-zA-Z0-9]" \
+		| grep -i "^| .* | .* | http.* | http.* | .* |$" >> "$arch".md
 	appnames=$(awk -F'|' '{print $2}' "$arch".md | grep -v -- "appname\|---" | tr ' ' '-' | sed -- 's/^-//g; s/-$//g')
 	for app in $appnames; do
 		app_lower=$(echo "$app" | tr '[:upper:]' '[:lower:]')
