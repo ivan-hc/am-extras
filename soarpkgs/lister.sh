b9a217dceb634a581e7e71d44d1354e34ccea104
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-03-26 20:28:48 +0100

    Update lister.sh

--- soarpkgs/lister.sh
+++ soarpkgs/lister.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
 
 architectures="x86_64 aarch64"
+categories="bincache pkgcache"
 for arch in $architectures; do
-	echo "| appname | description | site | download | version |" > "$arch".md
+	rm -f "$arch".md
 	echo "| ------- | ----------- | ---- | -------- | ------- |" >> "$arch".md
-	curl -Ls "https://github.com/pkgforge/pkgcache/releases/download/metadata/$arch-Linux.json" | jq '.' \
+	for c in $categories; do
+		curl -Ls "https://github.com/pkgforge/$c/releases/download/metadata/$arch-Linux.json" | jq '.' \
 		| grep "{\|\"pkg_name\"\|\"description\"\|\"pkg_webpage\"\|\"download_url\"\|\"version\"" \
 		| sed 's/,$/ | /g' | xargs | tr '{}' '\n' | sed 's/ pkg_name:/|/g' | sort -u \
 		| sed 's/description:/ | /g; s/pkg_webpage:/ | /g; s/download_url:/ | /g; s/version:/ | /g; s/  / /g; s/  / /g' \
 		| sed 's/| |/|/g' | awk -F'|' '{print $1 $2 $4 $3 $6 $5}' | sed 's/  / | /g; s/^/|/g; s/$/|/g' \
 		| grep -v "| |\|||\||[a-zA-Z0-9]" | grep -vi "/appimage/" \
 		| grep -i "^| .* | .* | http.* | http.*download.* | .* |$" | sort -u >> "$arch".md
+	done
+	list=$(sort "$arch".md)
+	printf "| appname | description | site | download | version |\n%b" "$list" > "$arch".md
 done
