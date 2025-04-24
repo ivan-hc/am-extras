9c2dd5916dd4724d51f7b2182fbd9b38e1934335
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-04-24 14:10:19 +0200

    Update lister.sh - wip
    - some references have to be refined

--- appbundlehub/lister.sh
+++ appbundlehub/lister.sh
@@ -4,19 +4,13 @@ architectures="x86_64"
 for arch in $architectures; do
 	rm -f "$arch".md
 	echo "| ------- | ----------- | ---- | -------- | ------- |" >> "$arch".md
-	curl -Ls "$(curl -Ls https://api.github.com/repos/xplshn/AppBundleHUB/releases | sed 's/[()",{} ]/\n/g' | grep -oi "http.*download.*metadata_$arch-Linux.json$" | head -1)" \
-		| grep "{\|\"pkg_name\"\|\"description\"\|\"homepage\"\|\"download_url\"\|\"version\"" \
-		| sed 's/,$/ | /g' | xargs | tr '{}' '\n' | sed 's/ pkg_name:/|/g' | sort -u \
-		| sed 's/description:/ | /g; s/homepage:/ | /g; s/download_url:/ | /g; s/version:/ | /g; s/  / /g; s/  / /g; s/ (may be inaccurate)//g' \
-		| sed 's/| |/|/g' | awk -F'|' '{print $1 $2 $3 $6 $5 $4}' | sed 's/  / | /g; s/^/|/g; s/$/|/g' \
-		| grep -v "| |\|||\||[a-zA-Z0-9]" \
-		| grep -i "^| .* | .* | http.* | http.*download.* | .* |$" | sort -u >> "$arch".md
-	appnames=$(awk -F'|' '{print $2}' "$arch".md | grep -v -- "appname\|---" | tr ' ' '-' | sed -- 's/^-//g; s/-$//g')
+	json_file=$(curl -Ls "$(curl -Ls https://api.github.com/repos/xplshn/AppBundleHUB/releases | sed 's/[()",{} ]/\n/g' | grep -oi "http.*download.*metadata_$arch-Linux.json$" | head -1)")
+	pkg_and_dl=$(echo "$json_file "| grep "{\|\"pkg\"\|\"download_url\"" | sed 's/,$/ | /g' | xargs | tr '{}' '\n' | sed 's/ pkg:/|/g; s/ download_url://g' | sort -u)
+	appnames=$(echo "$pkg_and_dl" | sed 's/ | | /\n/g' | awk -F'|' '{print $2}' | sed 's/.dwfs.AppBundle//g; s/.AppDir//g; s/.AppBundle//g' | tr '[:upper:]' '[:lower:]')
 	for app in $appnames; do
-		app_lower=$(echo "$app" | tr '[:upper:]' '[:lower:]')
-		pure_app=$(echo "$app" | tr '-' ' ')
-		sed -i "s/^| $pure_app |/| $app_lower |/g" "$arch".md
+		download=$(echo "$pkg_and_dl" | tr ' ' '\n' | grep -i "^https.*download.*/$app.")
+		echo "| $app | $description | $site | $download | $version |" >> "$arch".md
 	done
-	list=$(sort "$arch".md)
+	list=$(sort -u "$arch".md | grep "^|")
 	printf "| appname | description | site | download | version |\n%b" "$list" > "$arch".md
 done
