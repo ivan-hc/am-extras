fa02ffea31fe52fbe72d6d3681723666582ebb40
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-04-26 19:51:25 +0200

    Update lister.sh
    ...filter profiles I'm involved in: ivan-hc (me) and 0ad-matters

--- soarpkgs/lister.sh
+++ soarpkgs/lister.sh
@@ -11,7 +11,7 @@ for arch in $architectures; do
 		| sed 's/,$/ | /g' | xargs | tr '{}' '\n' | sed 's/ pkg_name:/|/g' | sort -u \
 		| sed 's/description:/ | /g; s/pkg_webpage:/ | /g; s/download_url:/ | /g; s/version:/ | /g; s/  / /g; s/  / /g' \
 		| sed 's/| |/|/g' | awk -F'|' '{print $1 $2 $4 $3 $6 $5}' | sed 's/  / | /g; s/^/|/g; s/$/|/g' \
-		| grep -v "| |\|||\||[a-zA-Z0-9]" | grep -vi "/nixappimage/" \
+		| grep -v "| |\|||\||[a-zA-Z0-9]" | grep -vi "/nixappimage/" | grep -vi "0ad-matters\|ivan-hc" \
 		| grep -i "^| .* | .* | http.* | http.*download.* | .* |$" | sort -u >> "$arch".md
 	done
 	list=$(sort -u "$arch".md)
