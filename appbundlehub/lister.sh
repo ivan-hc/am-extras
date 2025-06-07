7107fa6615dd256e46a642a2526df259484799fe
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-06-07 15:17:25 +0200

    Update lister.sh

--- appbundlehub/lister.sh
+++ appbundlehub/lister.sh
@@ -5,7 +5,7 @@ for arch in $architectures; do
 	rm -f "$arch".md
 	echo "| ------- | ----------- | ---- | -------- | ------- |" >> "$arch".md
 	json_file=$(curl -Ls "$(curl -Ls https://api.github.com/repos/xplshn/AppBundleHUB/releases | sed 's/[()",{} ]/\n/g' | grep -oi "http.*download.*metadata_$arch-Linux.json$" | head -1)")
-	pkg_and_dl=$(echo "$json_file "| grep "{\|\"pkg\"\|\"download_url\"" | sed 's/,$/ | /g' | xargs | tr '{}' '\n' | sed 's/ pkg:/|/g; s/ download_url://g' | sort -u)
+	pkg_and_dl=$(echo "$json_file " | grep "{\|\"pkg\"\|\"download_url\"" | sed 's/,$/ | /g' | xargs | tr '{}' '\n' | sed 's/ pkg:/|/g; s/ download_url://g' | sort -u)
 	appnames=$(echo "$pkg_and_dl" | sed 's/ | | /\n/g' | awk -F'|' '{print $2}' | sed -- 's/.dwfs.AppBundle//g; s/.sqfs.AppBundle//g; s/.AppDir//g; s/.AppBundle//g; s/-[0-9].*$//' | tr '[:upper:]' '[:lower:]')
 	for app in $appnames; do
 		appname="$app"
@@ -23,10 +23,19 @@ for arch in $architectures; do
 			archpage=$(curl -Ls https://aur.archlinux.org/packages/"$app")
 		elif curl -Ls https://aur.archlinux.org/packages/"$app"-git | grep -q "Description:"; then
 			archpage=$(curl -Ls https://aur.archlinux.org/packages/"$app"-git)
+		elif curl -Ls https://man.archlinux.org/man/"$app" | grep -q "DESCRIPTION"; then
+			archpage=$(curl -Ls https://man.archlinux.org/man/"$app")
 		fi
-		description=$(echo "$archpage" | grep -A 2 Description: | tr '><' '\n' | grep "^[A-Z]" | sed 's/&amp\;/and/g' | tail -1)
-		site=$(echo "$archpage" | grep -A 2 "Upstream URL" | tr '><' '\n' | grep "^http.*" | tail -1)
-		#version=$(echo "$archpage")
+		if echo "$archpage" | grep -q "DESCRIPTION"; then
+			description=$(echo "$archpage" | grep -A 2 NAME 2>/dev/null | grep "^<p " | head -1 | sed 's#</p>$##g' | tr '>' '\n' | tail -1 | cut -d" "  -f3-)
+			[ -n "$description" ] && description=$(echo "$description" | sed 's/.*/\u&/')
+			site=$(echo "$archpage" | grep -A 2 Upstream 2>/dev/null | tr '">< ' '\n' | grep -i "^http\|^ftp" | head -1)
+			#version=$(echo "$archpage" | grep -A 1 Version 2>/dev/null | grep "<dd>" | tr '">< ' '\n' | grep "^[0-9]" | tail -1)
+		else
+			description=$(echo "$archpage" | grep -A 2 Description: | tr '><' '\n' | grep "^[A-Z]" | sed 's/&amp\;/and/g' | tail -1)
+			site=$(echo "$archpage" | grep -A 2 "Upstream URL" | tr '><' '\n' | grep "^http.*" | tail -1)
+			#version=$(echo "$archpage")
+		fi
 		[ -z "$description" ] && description="No description available"
 		[ -z "$site" ] && site="https://xplshn.github.io/AppBundleHUB#$appname"
 		[ -z "$version" ] && version=$(echo "$download" | tr '/' '\n' | tail -2 | head -1)
