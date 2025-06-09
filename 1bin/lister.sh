b1367d1825d848dfcd238ce986843c8d06dbf7f4
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-06-09 16:40:25 +0200

    Update lister.sh

--- 1bin/lister.sh
+++ 1bin/lister.sh
@@ -12,8 +12,8 @@ for arch in $architectures; do
 		download=$(echo "$pkg_and_dl" | tr ' ' '\n' | grep -i "^https.*download.*/$app$")
 		if [ -f ../descriptions.md ] && grep -q "| $app |" ../descriptions.md; then
 			description=$(grep "^| $app |" ../descriptions.md | awk -F'|' '{print $3}' | sed 's/^ //g; s/ $//g; s/  / /g')
-			#site=$(grep "^| $app " ../descriptions.md | awk -F'|' '{print $4}' | sed 's/^ //g; s/ $//g; s/  / /g')
-			#[ "$site" = "None" ] && site=""
+			site=$(grep "^| $app " ../descriptions.md | awk -F'|' '{print $4}' | sed 's/^ //g; s/ $//g; s/  / /g')
+			[ "$site" = "None" ] && site=""
 		else
 			# Determine Arch Linux web page
 			if curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"/ | grep -q "Description:"; then
@@ -34,11 +34,11 @@ for arch in $architectures; do
 			if echo "$archpage" | grep -q "DESCRIPTION"; then
 				description=$(echo "$archpage" | grep -A 2 NAME 2>/dev/null | grep "^<p " | head -1 | sed 's#</p>$##g' | tr '>' '\n' | tail -1 | cut -d" "  -f3-)
 				[ -n "$description" ] && description=$(echo "$description" | sed 's/.*/\u&/')
-				#site=$(echo "$archpage" | grep -A 2 Upstream 2>/dev/null | tr '">< ' '\n' | grep -i "^http\|^ftp" | head -1)
+				site=$(echo "$archpage" | grep -A 2 Upstream 2>/dev/null | tr '">< ' '\n' | grep -i "^http\|^ftp" | head -1)
 				#version=$(echo "$archpage" | grep -A 1 Version 2>/dev/null | grep "<dd>" | tr '">< ' '\n' | grep "^[0-9]" | tail -1)
 			else
 				description=$(echo "$archpage" | grep -A 2 Description: | tr '><' '\n' | grep "^[A-Z]" | sed 's/&amp\;/and/g' | tail -1)
-				#site=$(echo "$archpage" | grep -A 2 "Upstream URL" | tr '><' '\n' | grep "^http.*" | tail -1)
+				site=$(echo "$archpage" | grep -A 2 "Upstream URL" | tr '><' '\n' | grep "^http.*" | tail -1)
 				#version=$(echo "$archpage")
 			fi
 		fi
