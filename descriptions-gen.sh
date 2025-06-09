af750e5b5f7b2d1a9f171f64b521673551877872
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-06-09 19:44:13 +0200

    Update descriptions-gen.sh - add more filters

--- descriptions-gen.sh
+++ descriptions-gen.sh
@@ -22,11 +22,11 @@ for app in $appnames; do
 			archpage=$(curl -Ls https://man.archlinux.org/man/"$app")
 		fi
 		if echo "$archpage" | grep -q "DESCRIPTION"; then
-			description=$(echo "$archpage" | grep -A 2 NAME 2>/dev/null | grep "^<p " | head -1 | sed 's#</p>$##g' | sed "s/&amp\;/and/g; s/&#39\;/\'/g" | tr '>' '\n' | tail -1 | cut -d" "  -f3-)
+			description=$(echo "$archpage" | grep -A 2 NAME 2>/dev/null | grep "^<p " | head -1 | sed 's#</p>$##g' | sed "s/&amp\;/and/g; s/&#39\;/\'/g; s/&#x00B4\;/\'/g; s/&lt\;//g; s/&#x27\;//g; s/&gt\;//g" | tr '>' '\n' | tail -1 | cut -d" "  -f3-)
 			[ -n "$description" ] && description=$(echo "$description" | sed 's/.*/\u&/')
 			site=$(echo "$archpage" | grep -A 2 Upstream 2>/dev/null | tr '">< ' '\n' | grep -i "^http\|^ftp" | head -1)
 		else
-			description=$(echo "$archpage" | grep -A 2 Description: | tr '><' '\n' | grep "^[A-Z]" | sed "s/&amp\;/and/g; s/&#39\;/\'/g" | tail -1)
+			description=$(echo "$archpage" | grep -A 2 Description: | tr '><' '\n' | grep "^[A-Z]" | sed "s/&amp\;/and/g; s/&#39\;/\'/g; s/&#x00B4\;/\'/g; s/&lt\;//g; s/&#x27\;//g; s/&gt\;//g" | tail -1)
 			site=$(echo "$archpage" | grep -A 2 "Upstream URL" | tr '><' '\n' | grep "^http.*" | tail -1)
 		fi
 		[ -z "$description" ] && description="No description available"
