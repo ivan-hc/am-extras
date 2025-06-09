1fb0befe92d5df3de398bd9bfca2646f851e43d7
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-06-09 14:53:32 +0200

    Update lister.sh - detect local descriptions

--- appbundlehub/lister.sh
+++ appbundlehub/lister.sh
@@ -10,31 +10,37 @@ for arch in $architectures; do
 	for app in $appnames; do
 		appname="$app"
 		download=$(echo "$pkg_and_dl" | tr ' ' '\n' | grep -i "^https.*download.*/$app.")
-		# Determine Arch Linux web page
-		if curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"/ | grep -q "Description:"; then
-			archpage=$(curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"/)
-		elif curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"-desktop/ | grep -q "Description:"; then
-			archpage=$(curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"-desktop/)
-		elif curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"/ | grep -q "Description:"; then
-			archpage=$(curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"/)
-		elif curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"-desktop/ | grep -q "Description:"; then
-			archpage=$(curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"-desktop/)
-		elif curl -Ls https://aur.archlinux.org/packages/"$app" | grep -q "Description:"; then
-			archpage=$(curl -Ls https://aur.archlinux.org/packages/"$app")
-		elif curl -Ls https://aur.archlinux.org/packages/"$app"-git | grep -q "Description:"; then
-			archpage=$(curl -Ls https://aur.archlinux.org/packages/"$app"-git)
-		elif curl -Ls https://man.archlinux.org/man/"$app" | grep -q "DESCRIPTION"; then
-			archpage=$(curl -Ls https://man.archlinux.org/man/"$app")
-		fi
-		if echo "$archpage" | grep -q "DESCRIPTION"; then
-			description=$(echo "$archpage" | grep -A 2 NAME 2>/dev/null | grep "^<p " | head -1 | sed 's#</p>$##g' | tr '>' '\n' | tail -1 | cut -d" "  -f3-)
-			[ -n "$description" ] && description=$(echo "$description" | sed 's/.*/\u&/')
-			site=$(echo "$archpage" | grep -A 2 Upstream 2>/dev/null | tr '">< ' '\n' | grep -i "^http\|^ftp" | head -1)
-			#version=$(echo "$archpage" | grep -A 1 Version 2>/dev/null | grep "<dd>" | tr '">< ' '\n' | grep "^[0-9]" | tail -1)
+		if [ -f ../descriptions.md ] && grep -q "| $app |" ../descriptions.md; then
+			description=$(grep "^| $app |" ../descriptions.md | awk -F'|' '{print $3}' | sed 's/^ //g; s/ $//g; s/  / /g')
+			site=$(grep "^| $app " ../descriptions.md | awk -F'|' '{print $4}' | sed 's/^ //g; s/ $//g; s/  / /g')
+			[ "$site" = "None" ] && site=""
 		else
-			description=$(echo "$archpage" | grep -A 2 Description: | tr '><' '\n' | grep "^[A-Z]" | sed 's/&amp\;/and/g' | tail -1)
-			site=$(echo "$archpage" | grep -A 2 "Upstream URL" | tr '><' '\n' | grep "^http.*" | tail -1)
-			#version=$(echo "$archpage")
+			# Determine Arch Linux web page
+			if curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"/ | grep -q "Description:"; then
+				archpage=$(curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"/)
+			elif curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"-desktop/ | grep -q "Description:"; then
+				archpage=$(curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"-desktop/)
+			elif curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"/ | grep -q "Description:"; then
+				archpage=$(curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"/)
+			elif curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"-desktop/ | grep -q "Description:"; then
+				archpage=$(curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"-desktop/)
+			elif curl -Ls https://aur.archlinux.org/packages/"$app" | grep -q "Description:"; then
+				archpage=$(curl -Ls https://aur.archlinux.org/packages/"$app")
+			elif curl -Ls https://aur.archlinux.org/packages/"$app"-git | grep -q "Description:"; then
+				archpage=$(curl -Ls https://aur.archlinux.org/packages/"$app"-git)
+			elif curl -Ls https://man.archlinux.org/man/"$app" | grep -q "DESCRIPTION"; then
+				archpage=$(curl -Ls https://man.archlinux.org/man/"$app")
+			fi
+			if echo "$archpage" | grep -q "DESCRIPTION"; then
+				description=$(echo "$archpage" | grep -A 2 NAME 2>/dev/null | grep "^<p " | head -1 | sed 's#</p>$##g' | tr '>' '\n' | tail -1 | cut -d" "  -f3-)
+				[ -n "$description" ] && description=$(echo "$description" | sed 's/.*/\u&/')
+				site=$(echo "$archpage" | grep -A 2 Upstream 2>/dev/null | tr '">< ' '\n' | grep -i "^http\|^ftp" | head -1)
+				#version=$(echo "$archpage" | grep -A 1 Version 2>/dev/null | grep "<dd>" | tr '">< ' '\n' | grep "^[0-9]" | tail -1)
+			else
+				description=$(echo "$archpage" | grep -A 2 Description: | tr '><' '\n' | grep "^[A-Z]" | sed 's/&amp\;/and/g' | tail -1)
+				site=$(echo "$archpage" | grep -A 2 "Upstream URL" | tr '><' '\n' | grep "^http.*" | tail -1)
+				#version=$(echo "$archpage")
+			fi
 		fi
 		[ -z "$description" ] && description="No description available"
 		[ -z "$site" ] && site="https://xplshn.github.io/AppBundleHUB#$appname"
