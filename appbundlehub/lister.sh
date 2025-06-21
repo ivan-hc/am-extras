0a93bbc0785a1d8931b20a92e466f2a5d7bc9649
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-06-21 18:46:44 +0200

    Update lister.sh - delegate descriptions to the parent file

--- appbundlehub/lister.sh
+++ appbundlehub/lister.sh
@@ -22,33 +22,6 @@ for arch in $architectures; do
 				site=$(grep "^| $app " ../descriptions.md | awk -F'|' '{print $4}' | sed 's/^ //g; s/ $//g; s/  / /g')
 				[ "$site" = "None" ] && site=""
 			fi
-		else
-			# Determine Arch Linux web page
-			if curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"/ | grep -q "Description:"; then
-				archpage=$(curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"/)
-			elif curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"-desktop/ | grep -q "Description:"; then
-				archpage=$(curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"-desktop/)
-			elif curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"/ | grep -q "Description:"; then
-				archpage=$(curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"/)
-			elif curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"-desktop/ | grep -q "Description:"; then
-				archpage=$(curl -Ls https://archlinux.org/packages/core/"$arch"/"$app"-desktop/)
-			elif curl -Ls https://aur.archlinux.org/packages/"$app" | grep -q "Description:"; then
-				archpage=$(curl -Ls https://aur.archlinux.org/packages/"$app")
-			elif curl -Ls https://aur.archlinux.org/packages/"$app"-git | grep -q "Description:"; then
-				archpage=$(curl -Ls https://aur.archlinux.org/packages/"$app"-git)
-			elif curl -Ls https://man.archlinux.org/man/"$app" | grep -q "DESCRIPTION"; then
-				archpage=$(curl -Ls https://man.archlinux.org/man/"$app")
-			fi
-			if echo "$archpage" | grep -q "DESCRIPTION"; then
-				description=$(echo "$archpage" | grep -A 2 NAME 2>/dev/null | grep "^<p " | head -1 | sed 's#</p>$##g' | tr '>' '\n' | tail -1 | cut -d" "  -f3-)
-				[ -n "$description" ] && description=$(echo "$description" | sed 's/.*/\u&/')
-				[ "$custom" != "true" ] && site=$(echo "$archpage" | grep -A 2 Upstream 2>/dev/null | tr '">< ' '\n' | grep -i "^http\|^ftp" | head -1)
-				#version=$(echo "$archpage" | grep -A 1 Version 2>/dev/null | grep "<dd>" | tr '">< ' '\n' | grep "^[0-9]" | tail -1)
-			else
-				description=$(echo "$archpage" | grep -A 2 Description: | tr '><' '\n' | grep "^[A-Z]" | sed 's/&amp\;/and/g' | tail -1)
-				[ "$custom" != "true" ] && site=$(echo "$archpage" | grep -A 2 "Upstream URL" | tr '><' '\n' | grep "^http.*" | tail -1)
-				#version=$(echo "$archpage")
-			fi
 		fi
 		[ -z "$description" ] && description="No description available"
 		[ -z "$site" ] && site="$ref#$appname"
