abbcbe1ffafd4324afbc345068de02ec743dc81b
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-06-23 03:11:19 +0200

    Using Debian man pages as a last (but essential) resort

--- descriptions-gen.sh
+++ descriptions-gen.sh
@@ -6,9 +6,7 @@ for app in $appnames; do
 	appname="$app"
 	if ! grep -q "^| $app | [A-Z].* |$" ./descriptions.md; then
 		# Determine Arch Linux web page
-		if curl -Ls https://manpages.debian.org/testing/"$app" | grep -q "DESCRIPTION"; then
-			manpage=$(curl -Ls https://manpages.debian.org/testing/"$app")
-		elif curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"/ | grep -q "Description:"; then
+		if curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"/ | grep -q "Description:"; then
 			manpage=$(curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"/)
 		elif curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"-desktop/ | grep -q "Description:"; then
 			manpage=$(curl -Ls https://archlinux.org/packages/extra/"$arch"/"$app"-desktop/)
@@ -22,14 +20,23 @@ for app in $appnames; do
 			manpage=$(curl -Ls https://aur.archlinux.org/packages/"$app"-git)
 		elif curl -Ls https://man.archlinux.org/man/"$app" | grep -q "DESCRIPTION"; then
 			manpage=$(curl -Ls https://man.archlinux.org/man/"$app")
+		elif curl -Ls https://manpages.debian.org/testing/"$app" | grep -q "DESCRIPTION"; then
+			manpage=$(curl -Ls https://manpages.debian.org/testing/"$app")
 		fi
 		if echo "$manpage" | grep -q "DESCRIPTION"; then
 			description=$(echo "$manpage" | grep -A 2 NAME 2>/dev/null | grep "^<p " | head -1 | sed 's#</p>$##g' | sed -- "s/&amp\;/and/g; s/&#39\;/\'/g; s/&#x00B4\;/\'/g; s/&lt\;//g; s/&#x27\;//g; s/&gt\;//g; s/ - //g" | tr '>' '\n' | tail -1 | cut -d" "  -f3-)
 			[ -n "$description" ] && description=$(echo "$description" | sed 's/.*/\u&/')
 			site=$(echo "$manpage" | grep -A 2 Upstream 2>/dev/null | tr '">< ' '\n' | grep -i "^http\|^ftp" | head -1)
+			[ -z "$site" ] && site=$(echo "$manpage" | grep -A 200 "DESCRIPTION" | tr '"><=' '\n' | grep "^http.*" | head -1)
 		else
 			description=$(echo "$manpage" | grep -A 2 Description: | tr '><' '\n' | grep "^[A-Z]" | sed -- "s/&amp\;/and/g; s/&#39\;/\'/g; s/&#x00B4\;/\'/g; s/&lt\;//g; s/&#x27\;//g; s/&gt\;//g; s/ - //g" | tail -1)
 			site=$(echo "$manpage" | grep -A 2 "Upstream URL" | tr '><' '\n' | grep "^http.*" | tail -1)
+			if [ -z "$site" ]; then
+				if curl -Ls https://manpages.debian.org/testing/"$app" | grep -q "DESCRIPTION"; then
+					manpage=$(curl -Ls https://manpages.debian.org/testing/"$app")
+					site=$(echo "$manpage" | grep -A 200 "DESCRIPTION" | tr '"><=' '\n' | grep "^http.*" | head -1)
+				fi
+			fi
 		fi
 		[ -z "$description" ] && description="No description available"
 		[ -z "$site" ] && site="None"
