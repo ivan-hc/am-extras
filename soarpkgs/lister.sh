a93e28328cbf3322f992851f7eadee83d6cc0b5a
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-07-28 12:10:57 +0200

    Update lister.sh

--- soarpkgs/lister.sh
+++ soarpkgs/lister.sh
@@ -4,19 +4,19 @@ CATEGORY="bincache pkgcache"
 header='| appname | description | site | download | version |
 | ------- | ----------- | ---- | -------- | ------- |'
 for arch in $ARCH; do
-  rows=$( for c in $CATEGORY; do
-      curl -fsSL "https://raw.githubusercontent.com/pkgforge/metadata/main/$c/data/${arch}-Linux.json" | \
-      awk '
-        /"pkg_name"/    { name = $0; gsub(/.*: *"/,"",name); gsub(/".*/,"",name) }
-        /"description"/ { desc = $0; gsub(/.*: *"/,"",desc); gsub(/".*/,"",desc) }
-        /"pkg_webpage"/ { site = $0; gsub(/.*: *"/,"",site); gsub(/".*/,"",site) }
-        /"ghcr_url"/{ dl  = $0; gsub(/.*: *"/,"",dl); gsub(/".*/,"",dl) }
-        /"version"/     { ver = $0; gsub(/.*: *"/,"",ver); gsub(/".*/,"",ver);
-          printf("| %s | %s | %s | %s | %s |\n", name, desc, site, dl, ver)
-        }' | grep -v pkgcache
-         done )
-{
-  printf '%s\n' "$header"
-  printf '%s\n' "$rows" | sort -u
-  } > "${arch}.md"
-			done
+	rows=$( for c in $CATEGORY; do
+	curl -fsSL "https://raw.githubusercontent.com/pkgforge/metadata/main/$c/data/${arch}-Linux.json" | jq '.' | \
+	awk '
+		/"pkg_name"/    { name = $0; gsub(/.*: *"/,"",name); gsub(/".*/,"",name) }
+		/"description"/ { desc = $0; gsub(/.*: *"/,"",desc); gsub(/".*/,"",desc) }
+		/"pkg_webpage"/ { site = $0; gsub(/.*: *"/,"",site); gsub(/".*/,"",site) }
+		/"ghcr_url"/{ dl  = $0; gsub(/.*: *"/,"",dl); gsub(/".*/,"",dl) }
+		/"version"/     { ver = $0; gsub(/.*: *"/,"",ver); gsub(/".*/,"",ver);
+		printf("| %s | %s | %s | %s | %s |\n", name, desc, site, dl, ver)
+		}' | grep -v pkgcache
+	done )
+	{
+	printf '%s\n' "$header"
+	printf '%s\n' "$rows" | sort -u
+	} > "${arch}.md"
+done
