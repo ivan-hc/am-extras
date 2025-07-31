3cb8d8b09d1d8a225826b5b929dbda817b1855e5
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-07-31 16:16:28 +0200

    Update soarpkgs - use "ghcr_blob" as download reference

--- soarpkgs/lister.sh
+++ soarpkgs/lister.sh
@@ -10,7 +10,7 @@ for arch in $ARCH; do
 		/"pkg_name"/    { name = $0; gsub(/.*: *"/,"",name); gsub(/".*/,"",name) }
 		/"description"/ { desc = $0; gsub(/.*: *"/,"",desc); gsub(/".*/,"",desc) }
 		/"pkg_webpage"/ { site = $0; gsub(/.*: *"/,"",site); gsub(/".*/,"",site) }
-		/"ghcr_url"/{ dl  = $0; gsub(/.*: *"/,"",dl); gsub(/".*/,"",dl) }
+		/"ghcr_blob"/{ dl  = $0; gsub(/.*: *"/,"",dl); gsub(/".*/,"",dl) }
 		/"version"/     { ver = $0; gsub(/.*: *"/,"",ver); gsub(/".*/,"",ver);
 		printf("| %s | %s | %s | %s | %s |\n", name, desc, site, dl, ver)
 		}' | grep -v "/appimage/\|nixappimage\|runimage\|appbundle\|flatimage" | \
@@ -20,4 +20,5 @@ for arch in $ARCH; do
 	printf '%s\n' "$header"
 	printf '%s\n' "$rows" | sort -u
 	} > "${arch}.md"
+	sed -i 's# ghcr.io/# https://ghcr.io/#g' "${arch}.md"
 done
