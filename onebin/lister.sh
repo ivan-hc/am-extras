8d0a7a29bcb1ac9c1bb3509d7694065a557b9e54
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-08-07 15:31:37 +0200

    Update lister.sh

--- onebin/lister.sh
+++ onebin/lister.sh
@@ -4,34 +4,30 @@ architectures="x86_64"
 custom="true"
 ref="https://1bin.org"
 
-_sources() {
-	source_list=$(curl -Ls https://api.github.com/repos/nikvdp/1bin/releases/latest | sed 's/[()",{} ]/\n/g')
-	pkg_and_dl=$(echo "$source_list " | grep -i "^http.*download*" | grep -v "/mac-")
-	appnames=$(echo "$pkg_and_dl" | sed 's:.*/::' | tr '[:upper:]' '[:lower:]')
-}
+header='| appname | description | site | download | version |
+| ------- | ----------- | ---- | -------- | ------- |'
 
-for arch in $architectures; do
-	rm -f "$arch".md
-	_sources
-	for app in $appnames; do
-		appname="$app"
-		download=$(echo "$pkg_and_dl" | tr ' ' '\n' | grep -i "^https.*download.*/$app$")
-		if [ -f ../descriptions.md ] && grep -q "| $app |" ../descriptions.md; then
-			description=$(grep "^| $app |" ../descriptions.md | awk -F'|' '{print $3}' | sed 's/^ //g; s/ $//g; s/  / /g')
-			if [ "$custom" != "true" ]; then
-				site=$(grep "^| $app " ../descriptions.md | awk -F'|' '{print $4}' | sed 's/^ //g; s/ $//g; s/  / /g')
-				[ "$site" = "None" ] && site=""
+source_list=$(curl -Ls https://api.github.com/repos/nikvdp/1bin/releases/latest | sed 's/[()",{} ]/\n/g')
+[ -z "$source_list" ] && source_list=$(curl -Ls https://api.gh.pkgforge.dev/repos/nikvdp/1bin/releases/latest | sed 's/[()",{} ]/\n/g')
+pkg_and_dl=$(echo "$source_list " | grep -i "^http.*download*" | grep -v "/mac-")
+appnames=$(echo "$pkg_and_dl" | sed 's:.*/::' | tr '[:upper:]' '[:lower:]')
+
+if [ -n "$source_list" ] && echo "$pkg_and_dl" | grep -qi "1bin"; then
+	for arch in $architectures; do
+		echo "$header" > "$arch".md
+		for app in $appnames; do
+			appname="$app"
+			if [ -f ../descriptions.md ] && grep -q "| $app |" ../descriptions.md; then
+				description=$(grep "^| $app |" ../descriptions.md | awk -F'|' '{print $3}' | sed 's/^ //g; s/ $//g; s/  / /g')
+				[ -z "$description" ] && description="No description available"
 			fi
-		fi
-		version=$(echo "$download" | sed 's:.*/::' | grep -oP '(?<=-)([0-9]+\.?)+' | sed 's/ //g' | head -1)
-		[ -z "$description" ] && description="No description available"
-		[ -z "$site" ] && site="$ref"
-		[ -z "$version" ] && version=$(echo "$download" | tr '/' '\n' | tail -2 | head -1)
-		echo "| $appname | $description | $site | $download | $version |" >> "$arch".md
-		unset appname description site download	version archpage
+			site="$ref"
+			download=$(echo "$pkg_and_dl" | tr ' ' '\n' | grep -i "^https.*download.*/$app$")
+			version=$(echo "$download" | tr '/' '\n' | tail -2 | head -1)
+			if echo "| $appname | $description | $site | $download | $version |" | grep -qi "^| .* | .* | http.* | http.*download.* | .* |$"; then
+				echo "| $appname | $description | $site | $download | $version |" >> "$arch".md
+			fi
+			unset appname description site download	version
+		done
 	done
-	list=$(sort -u "$arch".md | grep -i "^| .* | .* | http.* | http.*download.* | .* |$")
-	echo "| appname | description | site | download | version |" > "$arch".md
-	echo "| ------- | ----------- | ---- | -------- | ------- |" >> "$arch".md
-	echo "$list" >> "$arch".md
-done
+fi
